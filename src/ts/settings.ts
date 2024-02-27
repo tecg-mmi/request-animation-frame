b0dbd34ab69127202d2e52c134526ce20b70f84d
Daniel Schreurs <daniel.schreurs@hotmail.com>
2024-02-27 18:36:42 +0100

    Add settings

---
+++ src/ts/settings.ts
@@ -0,0 +1,37 @@
+export const settings = {
+    monster: {
+        width:27,
+        height:29,
+        frames: [{
+            sx: 104,
+            sy: 16,
+            sw: 27,
+            sh: 29,
+        },{
+            sx: 132,
+            sy: 16,
+            sw: 27,
+            sh: 29,
+        },{
+            sx: 160,
+            sy: 16,
+            sw: 27,
+            sh: 29,
+        },{
+            sx: 188,
+            sy: 16,
+            sw: 27,
+            sh: 29,
+        },{
+            sx: 216,
+            sy: 16,
+            sw: 27,
+            sh: 29,
+        },{
+            sx: 244,
+            sy: 16,
+            sw: 27,
+            sh: 29,
+        }]
+    }
+}
