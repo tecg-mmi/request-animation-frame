9cf9005fcac560d4126cc1746d57270f6fd615a2
Daniel Schreurs <DanielSchreurs@users.noreply.github.com>
2025-03-17 10:51:20 +0100

    Initial commit

--- src/ts/helper.ts
+++
@@ -1,9 +0,0 @@
-export function random(min: number, max: number): number {
-    return Math.floor(Math.random() * (max - min) + min);
-}
-
-export function checkCollision(ax1: number, ax2: number, ay1: number, ay2: number,
-                               bx1: number, bx2: number, by1: number, by2: number,): boolean {
-    return !(ax2 < bx1 || ay2 < by1 || ax1 > bx2 || ay1 > by2);
-}
-
